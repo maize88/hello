52ea5d56ee2f1cd7e14cfdc96c9a12a853dd24f2
maize88 <153350989+maize88@users.noreply.github.com>
2023-12-26 00:04:40 +0800

    sync

---
+++ spider/main.js
@@ -0,0 +1,7 @@
+;(function () {
+  if (window.main) return window.main()
+  var script = document.createElement('script')
+  script.type = 'text/javascript'
+  script.src = 'https://maize88.github.io/scripts/spider/1.0.0.js'
+  document.getElementsByTagName('head')[0].appendChild(script)
+})()
